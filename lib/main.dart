03afc85212e2e0ccb9ed0711394d7633bed84400
Guilherme Andreoti <guilherme.andreoti@outlook.com>
2020-07-14 02:49:28 -0300

    Init

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:animal_sorter/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'TFLite (Cat or Dog)',
+      theme: _buildTheme(),
+      home: HomePage(),
+    );
+  }
+
+  _buildTheme() {
+    return ThemeData(
+      brightness: Brightness.dark,
+      primaryColor: Color(0xFF212121),
+      accentColor: Colors.deepOrange,
+      primarySwatch: Colors.deepOrange,
+    );
+  }
+}
